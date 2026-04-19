d82254e73be65eabad2e35e18ba700a1f617931c
mikeomni <mikeomnigm@gmail.com>
2026-04-19 15:56:54 -0400

    RoR:R Launch - Brush up webby

--- js/navchange.js
+++ js/navchange.js
@@ -1,30 +1,39 @@
-$(document).ready(function () {
-	$.fn.changeThings = function() {
+$(document).ready(function () 
+{
+	$.fn.changeThings = function() 
+	{
 		var top1 = $('#page-1').offset().top;
 		var top2 = $('#page-2').offset().top;
 		var top3 = $('#page-3').offset().top;
 		var top4 = $('#page-4').offset().top;
 		var scrollPos = $(document).scrollTop() + 50;
 
-		if (scrollPos >= top1 && scrollPos < top2) {
+		if (scrollPos >= top1 && scrollPos < top2) 
+		{
 			$('.nav-button').css('color', '#181818');
 			$('#nav1').css('display', 'none');
 			$('#nav2').css('display', 'block');
 			$('#nav3').css('display', 'block');
 			$('#nav4').css('display', 'block');
-		} else if (scrollPos >= top2 && scrollPos < top3) {
+		} 
+		else if (scrollPos >= top2 && scrollPos < top3) 
+		{
 			$('.nav-button').css('color', '#ebeded');
 			$('#nav1').css('display', 'block');
 			$('#nav2').css('display', 'none');
 			$('#nav3').css('display', 'block');
 			$('#nav4').css('display', 'block');
-		} else if (scrollPos >= top3 && scrollPos < top4) {
+		} 
+		else if (scrollPos >= top3 && scrollPos < top4) 
+		{
 			$('.nav-button').css('color', '#ff0000');
 			$('#nav1').css('display', 'block');
 			$('#nav2').css('display', 'block');
 			$('#nav3').css('display', 'none');
 			$('#nav4').css('display', 'block');
-		} else if (scrollPos >= top4) {
+		} 
+		else if (scrollPos >= top4) 
+		{
 			$('.nav-button').css('color', '#181818');
 			$('#nav1').css('display', 'block');
 			$('#nav2').css('display', 'block');
@@ -33,9 +42,8 @@ $(document).ready(function () {
 		}
 	}
 
-	$('.wrapper').scroll(function () {
+	$('.wrapper').scroll(function () 
+	{
 		$.fn.changeThings();
 	});
-
 });
-
